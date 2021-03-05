1c1ca3dfc646e9e3deaac2047a2e2c91a354981a
IAbrahamI <abraham.leguizamon@hotmail.com>
2021-03-05 22:39:31 +0100

    added radioButtons

--- main/Brett.js
+++ main/Brett.js
@@ -3,46 +3,42 @@ const ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
 let x = 0;
 let y = 0;
+let width = 0;
+let counter = 0;
 let textInput=[];
 let listOfWhite=[];
+const valueRect = [];
+let amountOfRects = 0;
 //------------------------------------------------------------
 // Methode für die Random Erstellung von Schwarze Rectangles
-const amountOfRds = 64;
-const valueRect = [];
-for(let l=0; l<amountOfRds; l++){
-    const randomNumber = Math.floor(Math.random()*4);
-    valueRect.push(randomNumber);
-}
-//------------------------------------------------------------
-// Methode zum Zeichnen des Bretts mit schwarze Rechtecke
-let amountOfrect = 0;
-for(let i=0; i<8; i++){
-    for(let j=0; j<8; j++){
-        ctx.strokeRect(x,y,60,60);
-        if(valueRect[amountOfrect]===0 || valueRect[amountOfrect]===1 || valueRect[amountOfrect]===2){
-            ctx.fillRect(x,y,60,60);
-        } else if(valueRect[amountOfrect]===3){
-            listOfWhite.push(amountOfrect);
-        }
-        amountOfrect++;
-        x+=60;
-        if(j===7){
-            x=0;
-            y+=60;
-        }
+function addRandomValues(){
+    const amountOfRds = width*width;
+    for(let l=0; l<amountOfRds; l++){
+        const randomNumber = Math.floor(Math.random()*4);
+        valueRect.push(randomNumber);
     }
 }
 //------------------------------------------------------------
-// Button Funktion --Zeichnet alle Buchstaben
-function saveValue(){
-    const inputValue = document.getElementById('input').value;
+// Speichert und zeichnet alle Buchstaben
+function saveValues(){
+    const inputValue = document.getElementById('textInput').value;
+    const valueForWidth = document.querySelectorAll('input[name="width"]');
+    for (const radioB of valueForWidth) {
+        if(radioB.checked){
+            width = radioB.value;
+            break;
+        }
+    }
     textInput = Array.from(inputValue);
-    let x = 0;
-    let y = 0;
-    let counter = 0;
-    let amountOfRects = 0;
-    for(let i=0; i<8; i++){
-        for(let j=0; j<8; j++){
+    addRandomValues();
+    for(let i=0; i<width; i++){
+        for(let j=0; j<width; j++){
+            ctx.strokeRect(x,y,60,60);
+            if(valueRect[amountOfRects]===0 || valueRect[amountOfRects]===1 || valueRect[amountOfRects]===2){
+                ctx.fillRect(x,y,60,60);
+            } else if(valueRect[amountOfRects]===3){
+                listOfWhite.push(amountOfRects);
+            }
             for (let k=0;k<listOfWhite.length;k++) {
                 if(listOfWhite[k]===amountOfRects){
                     if(textInput.length<=counter){
@@ -55,7 +51,7 @@ function saveValue(){
             }
             amountOfRects++;
             x+=60;
-            if(j===7){
+            if(j===(width-1)){
                 x=0;
                 y+=60;
             }
