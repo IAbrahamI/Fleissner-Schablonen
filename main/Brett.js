a85158782e34dbfbe8846a71b41b053016dd10ad
IAbrahamI <45631927+IAbrahamI@users.noreply.github.com>
2021-04-08 10:50:22 +0200

    changed storage array to a 3 dimensional arra

--- main/Brett.js
+++ main/Brett.js
@@ -5,6 +5,7 @@ let x = 0;
 let y = 0;
 let width = 4;
 let counter = 0;
+let matrix = [];
 let textInput=[];
 let listOfWhite=[];
 const valueRect = [];
@@ -18,33 +19,52 @@ function addRandomValues(){
         valueRect.push(randomNumber);
     }
 }
+function twoDimensional(){
+    for(let x=0;x<width;x++){
+        for(let y=0;y<width;y++){
+            if(valueRect[amountOfRects]===0 || valueRect[amountOfRects]===1 || valueRect[amountOfRects]===2){
+                console.log(valueRect[amountOfRects]);
+                matrix.push([x,y,0]);
+            } else if(valueRect[amountOfRects]===3){
+                console.log(valueRect[amountOfRects]);
+                matrix.push([x,y,1]);
+            }
+            amountOfRects++;
+        }
+    }
+}
 //------------------------------------------------------------
 // Speichert und zeichnet alle Buchstaben
 function saveValues(){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
     const inputValue = document.getElementById('textInput').value;
     textInput = Array.from(inputValue);
     if(textInput.length>(width*4)){
         width = width+2;
     }
     addRandomValues();
+    twoDimensional();
     for(let i=0; i<width; i++){
+        console.log(i);
         for(let j=0; j<width; j++){
             ctx.strokeRect(x,y,60,60);
-            if(valueRect[amountOfRects]===0 || valueRect[amountOfRects]===1 || valueRect[amountOfRects]===2){
-                ctx.fillRect(x,y,60,60);
-            } else if(valueRect[amountOfRects]===3){
-                listOfWhite.push(amountOfRects);
-            }
-            for (let k=0;k<listOfWhite.length;k++) {
-                if(listOfWhite[k]===amountOfRects){
-                    if(textInput.length<=counter){
-                        ctx.fillText("X",x+20,y+40);
-                    }else if(counter<=textInput.length){
-                        ctx.fillText(textInput[k],x+21,y+40);
-                        counter++;
-                    }
+            for(let k=0; i<matrix.length; i++){
+                if(matrix[k]==1){
+                    ctx.strokeRect(x,y,60,60);
+                }else if(matrix[k]==0){
+                    ctx.fillRect(x,y,60,60);
                 }
             }
+            // for (let k=0;k<listOfWhite.length;k++) {
+            //     if(listOfWhite[k]===amountOfRects){
+            //         if(textInput.length<=counter){
+            //             ctx.fillText("X",x+20,y+40);
+            //         }else if(counter<=textInput.length){
+            //             ctx.fillText(textInput[k],x+21,y+40);
+            //             counter++;
+            //         }
+            //     }
+            // }
             amountOfRects++;
             x+=60;
             if(j===(width-1)){
