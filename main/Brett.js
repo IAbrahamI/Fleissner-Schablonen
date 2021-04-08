b86090e2794447c841992257c0f1c1191b177de6
IAbrahamI <45631927+IAbrahamI@users.noreply.github.com>
2021-04-08 12:00:16 +0200

    updated matrix array to store right

--- main/Brett.js
+++ main/Brett.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 ctx.font = "30px Arial";
-let x = 0;
-let y = 0;
+let xForRect = 0;
+let yForRect = 0;
 let width = 4;
 let counter = 0;
 let matrix = [];
@@ -19,17 +19,47 @@ function addRandomValues(){
         valueRect.push(randomNumber);
     }
 }
+//------------------------------------------------------------
+// Methode um die Schablone zu zeichnen und Werte richtig zu speichern
 function twoDimensional(){
     for(let x=0;x<width;x++){
         for(let y=0;y<width;y++){
             if(valueRect[amountOfRects]===0 || valueRect[amountOfRects]===1 || valueRect[amountOfRects]===2){
-                console.log(valueRect[amountOfRects]);
                 matrix.push([x,y,0]);
+                ctx.fillRect(xForRect,yForRect,60,60);
+
             } else if(valueRect[amountOfRects]===3){
-                console.log(valueRect[amountOfRects]);
                 matrix.push([x,y,1]);
+                ctx.strokeRect(xForRect,yForRect,60,60);
             }
             amountOfRects++;
+            xForRect+=60;
+            if(y===(width-1)){
+                xForRect=0;
+                yForRect+=60;
+            }
+        }
+    }
+    amountOfRects=0;
+}
+
+//------------------------------------------------------------
+// Funktion um die Werte zu ersetzten
+function rotateValues(){
+    for(let i=0; i<width; i++){
+        for(let j=0; j<width; j++){
+            ctx.strokeRect(x,y,60,60);
+            if(matrix[amountOfRects][2]==0){
+                ctx.fillRect(x,y,60,60);
+            }else if(matrix[amountOfRects][2]==1){
+                ctx.strokeRect(x,y,60,60);
+            }
+            amountOfRects++;
+            x+=60;
+            if(j===(width-1)){
+                x=0;
+                y+=60;
+            }
         }
     }
 }
@@ -44,36 +74,8 @@ function saveValues(){
     }
     addRandomValues();
     twoDimensional();
-    for(let i=0; i<width; i++){
-        console.log(i);
-        for(let j=0; j<width; j++){
-            ctx.strokeRect(x,y,60,60);
-            for(let k=0; i<matrix.length; i++){
-                if(matrix[k]==1){
-                    ctx.strokeRect(x,y,60,60);
-                }else if(matrix[k]==0){
-                    ctx.fillRect(x,y,60,60);
-                }
-            }
-            // for (let k=0;k<listOfWhite.length;k++) {
-            //     if(listOfWhite[k]===amountOfRects){
-            //         if(textInput.length<=counter){
-            //             ctx.fillText("X",x+20,y+40);
-            //         }else if(counter<=textInput.length){
-            //             ctx.fillText(textInput[k],x+21,y+40);
-            //             counter++;
-            //         }
-            //     }
-            // }
-            amountOfRects++;
-            x+=60;
-            if(j===(width-1)){
-                x=0;
-                y+=60;
-            }
-        }
-    }
 }
 //------------------------------------------------------------
 function rotateCanvas(){
+    rotateValues();
 }
